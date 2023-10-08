1b6a30e47d65532ce8a7a8dd4f18b38eae7ccdec
Drounz <davidegorp@gmail.com>
2023-10-08 10:03:47 +0100

    fixed and added the image to it

---
+++ android/app/src/main/kotlin/com/example/twoam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twoam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/Logomuckpng.png", width: 200, height: 250),
+          ],
+        ),
+      ),
+    );
+  }
+}
