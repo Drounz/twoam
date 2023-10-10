805fb49db11b789cf54c69a19fd7b7f2d11392ed
Drounz <davidegorp@gmail.com>
2023-10-10 07:28:21 +0100

    i chnage the from and edded some enable and focused state

--- lib/name.dart
+++ lib/name.dart
@@ -1,5 +1,42 @@
 import 'package:flutter/material.dart';
 
+class UserName extends StatefulWidget {
+  const UserName({super.key});
+
+  @override
+  State<UserName> createState() => _UserNameState();
+}
+
+class _UserNameState extends State<UserName> {
+  TextEditingController _nameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 64,
+      child: TextFormField(
+        controller: _nameController,
+        decoration: InputDecoration(
+          hintText: "any name nah name sha",
+          labelText: 'Your guy name',
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4.0),
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4.0),
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class name_page extends StatefulWidget {
   const name_page({super.key});
 
@@ -25,18 +62,18 @@ class _name_pageState extends State<name_page> {
         padding: EdgeInsets.all(16.0),
         child: Column(children: [
           Padding(
-            padding: EdgeInsets.only(right: 215),
+            padding: EdgeInsets.only(right: 106),
             child: Text(
               "Who goes you?",
               style: TextStyle(
-                fontSize: 24,
+                fontSize: 40,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
               ),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 8.0),
+            padding: EdgeInsets.only(top: 16.0),
             child: Text(
               "This determins the kind of questions you will be asked to answer ohh, no dull",
               style: TextStyle(
@@ -45,6 +82,11 @@ class _name_pageState extends State<name_page> {
               ),
             ),
           ),
+          SizedBox(height: 48.0),
+          Padding(
+            padding: EdgeInsets.all(0.0),
+            child: UserName(),
+          )
         ]),
       ),
     );
