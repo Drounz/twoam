79ab48da3d6a3937c7bcaf9dd9e7118d1c5ac5ea
Drounz <davidegorp@gmail.com>
2023-11-06 10:40:01 +0100

    i clared all the old code so set alarm and i am trying to write a new one

--- lib/setalarm.dart
+++ lib/setalarm.dart
@@ -1,75 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:twoam/main.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+  await AndroidAlarmManager.initialize();
+}
 
 class SetAlarm extends StatefulWidget {
   const SetAlarm({super.key});
 
   @override
-  _SetAlarmState createState() => _SetAlarmState();
+  State<SetAlarm> createState() => _SetAlarmState();
 }
 
 class _SetAlarmState extends State<SetAlarm> {
-  TimeOfDay _selectedTime = TimeOfDay.now();
-  late DateTime alarmTime;
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-    );
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-        alarmTime = DateTime(
-          DateTime.now().year,
-          DateTime.now().month,
-          DateTime.now().day,
-          _selectedTime.hour,
-          _selectedTime.minute,
-        );
-      });
-    }
-  }
-
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Set your alarm"),
         backgroundColor: Colors.black,
-        title: Text("Set alarm"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Selected Time: ${_selectedTime.format(context)}',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _selectTime(context),
-              child: Text('Select Alarm Time'),
-            ),
-          ],
-          
-          // If the user selected a time, set the alarm.
-             
-
-if (_selectedTime != null) {
-  alarmTime = _selectedTime;
-  await setAlarm();
-
-  // Display a confirmation message.
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text('Alarm set for ${alarmTime.format(context)}'),
-    ),
-  );
-},
-
-        ),
       ),
     );
   }
